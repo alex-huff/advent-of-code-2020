4a91a1244e5fa308815098e4aa3a966f368ba22c
alex-huff <38389408+alex-huff@users.noreply.github.com>
2020-12-20 09:39:48 -0600

    optimize day 13 using periods of partial solutions

--- day13.py
+++ day13.py
@@ -25,25 +25,54 @@ print(earliestBid * earliestWait)
 
 # part 2
 
+# approach one, takes too long
+
+# busses = [int(bid) if bid != 'x' else -1 for bid in rawinput[1].split(',')]
+# maxbid = max(busses)
+# maxbidLoc = busses.index(maxbid)
+# i = maxbid - maxbidLoc
+# progress = 0
+# sortedBids = [bid for bid in busses if bid != -1]
+
+# sortedBids.sort()
+# sortedBids.reverse()
+
+# bidEntries = [[bid, busses.index(bid)] for bid in sortedBids]
+
+# while True:
+#     for bid, j in bidEntries:
+#         if (i + j) % bid != 0:
+#             break
+#     else:
+#         print(i)
+
+#         break
+
+#     i += maxbid
+
+# approach two
+
 busses = [int(bid) if bid != 'x' else -1 for bid in rawinput[1].split(',')]
-maxbid = max(busses)
-maxbidLoc = busses.index(maxbid)
-i = maxbid - maxbidLoc
-progress = 0
-sortedBids = [bid for bid in busses if bid != -1]
+bids = [bid for bid in busses if bid != -1]
+bidEntries = [[bid, busses.index(bid)] for bid in bids]
+firstOccurance = 0
+period = bidEntries[0][0]
 
-sortedBids.sort()
-sortedBids.reverse()
+for be in range(1, len(bidEntries)):
+    current = bidEntries[be]
+    currentLoc = firstOccurance
 
-bidEntries = [[bid, busses.index(bid)] for bid in sortedBids]
+    while (currentLoc + current[1]) % current[0] != 0:
+        currentLoc += period
 
-while True:
-    for bid, j in bidEntries:
-        if (i + j) % bid != 0:
-            break
-    else:
-        print(i)
+    firstOccurance = currentLoc
+    currentLoc += period
 
-        break
+    while (currentLoc + current[1]) % current[0] != 0:
+        currentLoc += period
 
-    i += maxbid
+    secondOccurance = currentLoc
+
+    period = secondOccurance - firstOccurance
+
+print(firstOccurance)
